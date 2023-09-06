52c0a365bd5f822004aff40c39606fdbdd7f374c
임효비 <hblim0909@naver.com>
2023-09-06 22:56:33 +0900

    first commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,35 @@
-import logo from './logo.svg';
 import './App.css';
+import React from "react";
+import {
+  BrowserRouter,
+  Routes,
+  Route
+} from "react-router-dom";
+import MainPage from "./page/MainPage";
+import SelectPage from "./page/SelectPage";
+import MyCheckPage from "./page/MyCheckPage";
+import ActionPage from "./page/ActionPage";
+import UploadActionPage from "./page/UploadActionPage";
+import SelectWayPage from "./page/SelectWayPage";
+import ShowWayPage from "./page/ShowWayPage";
+import MyResultPage from "./page/MyResultPage";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<MainPage />} />
+        <Route path="selectpage" element={<SelectPage />} />
+        <Route path="mycheckpage" element={<MyCheckPage />} />
+        <Route path="actionpage" element={<ActionPage />} />
+        <Route path="uploadactionpage" element={<UploadActionPage />} />
+        <Route path="selectwaypage" element={<SelectWayPage />} />
+        <Route path="showwaypage" element={<ShowWayPage />} />
+        <Route path="myresultpage" element={<MyResultPage />} />
+      </Routes>
+    </BrowserRouter>
+    </>
   );
 }
 
